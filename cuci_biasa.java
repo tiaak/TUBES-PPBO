91bea4ca2d2c6043553100accd938b27c0958925
tiaak <77487682+tiaak@users.noreply.github.com>
2022-04-25 21:57:58 +0700

    Update cuci_biasa.java

--- cuci_biasa.java
+++ cuci_biasa.java
@@ -3,9 +3,9 @@ package kasir_loundry;
 public class cuci_biasa extends cuci1 {
     private double berat;
 
-//    public void setBerat(int berat){
-//        this.berat = berat;
-//    }
+   public void setBerat(double berat){
+       this.berat = berat;
+   }
 
     public double getBerat(){
         return (double) berat*4000;
